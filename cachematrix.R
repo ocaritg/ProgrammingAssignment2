64998c522f37074e08d3ea45a2c3e4f84e2ec749
ocaritg <ocaritg@gmail.com>
2014-07-28 00:54:02 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL ## creates an empty object
+        set <- function(y) { ## create a function which.. 
+                x <<- y ## assigns y (a value) to x...
+                m <<- NULL ## and an empty object to m
+        }
+        get <- function() x ## returns x
+        setSolve <- function(solve) m <<- solve  ## inverts the matrix
+        getSolve <- function() m ## returns the inverted matrix
+        list(set = set, get = get, ## creates a list of all four functions
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
 
+## for proper use of the function, create a new object and assign this object the result of the previous function
+## example: matr<-makeMatrix(here_your_matrix)
+## then call cacheSolve(matr)
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...) { 
+	m <- x$getSolve() ## assigns the inverted matrix to m
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	if(!is.null(m)) { ## if m is not NULL i.e. was previosly calculated
+		message("getting cached data") ## gives the message
+		return(m) ## returns the inverted matrix
+	}
+	
+	else{ ## if m is NULL
+	data <- x$get() ## assigns the x value to a new object called data
+	m <- solve(data, ...) ## inverts the matrix and assigns it to m
+	x$setSolve(m) ## caches the newly inverted matrix  
+	m ## returns the inverted matrix
+	}
 }
